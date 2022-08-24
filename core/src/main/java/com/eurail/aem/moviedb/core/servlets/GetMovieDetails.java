de2385aea9861115061bd09fa89c18a95ed13590
swagato-dey <89976552+swagato-dey@users.noreply.github.com>
2022-08-24 19:56:28 +0530

    IMDB Search API Integrations

---
+++ core/src/main/java/com/eurail/aem/moviedb/core/servlets/GetMovieDetails.java
@@ -0,0 +1,67 @@
+package com.eurail.aem.moviedb.core.servlets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.HttpConstants;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletPaths;
+import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component(service = { Servlet.class })
+@SlingServletResourceTypes(
+		resourceTypes="moviedb/getmoviedetails",
+		methods=HttpConstants.METHOD_GET,
+		extensions="txt")
+public class GetMovieDetails extends SlingSafeMethodsServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private static final Logger LOG = LoggerFactory.getLogger(GetMovieDetails.class);
+
+	@Override
+	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
+			throws ServletException, IOException {
+		LOG.error("Inside Servlet");
+		String searchText = request.getParameter("searchText");
+		LOG.error("searchText : " + searchText);
+		// Create a neat value object to hold the URL
+		URL url = new URL("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+
+		// Open a connection(?) on the URL(??) and cast the response(???)
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+
+		// Now it's "open", we can set the request method, headers etc.
+		connection.setRequestProperty("accept", "application/json");
+
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(connection.getInputStream()));
+		String inputLine;
+		StringBuffer content = new StringBuffer();
+		while ((inputLine = in.readLine()) != null) {
+			content.append(inputLine);
+		}
+		in.close();
+		
+		LOG.error("Response : " + content.toString());
+
+	}
+}
